6ce5e47bf6acea1f5c3484a79a7937bd1b2a0624
Chris Scanlon <chris.scanlon72@gmail.com>
2018-01-09 17:46:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, {[key]: value});
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key) {
-  delete object[key];
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
   return object;
 }
