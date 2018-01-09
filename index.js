86f42279002ef5947af8b8afc657a96c011e5697
Chris Scanlon <chris.scanlon72@gmail.com>
2018-01-09 17:42:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assign({}, object, {[key]: [value]});
+  return Object.assign({}, object, {[key]: value});
 }
